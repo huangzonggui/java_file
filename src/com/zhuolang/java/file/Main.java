bc84f2b64b3a296407747e13fb1a84a943645e3c
zonggui <1226620085@qq.com>
2015-12-15 14:56:22 +0800

    功能未完善

--- src/com/zhuolang/java/file/Main.java
+++ src/com/zhuolang/java/file/Main.java
@@ -1,5 +1,7 @@
-package com.zhuolang.java.file;
+//package com.zhuolang.java.file;
 
+import java.io.*;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -31,7 +33,6 @@ public class Main {
 			for (;;) 
 			{
 			param.clear();
-			
 			System.out.print("["+name +"@"+path+" ~]$ ");
 			String in = reader.readLine();
 			String [] ins = in.split(" +"); //正则表达拆分，1个以上的空格
@@ -59,7 +60,13 @@ public class Main {
 			case "ls":
 				ls();
 				break;
-
+		
+			case "pwd":
+				pwd();
+				break;
+			case "copy":
+				copy();
+				break;
 			default:
 				System.out.println("你说什么啊，sb");
 				break;
@@ -72,14 +79,15 @@ public class Main {
 	private void cd(){
 		if(param.isEmpty()){
 			System.out.println("有病是不是，会不会用呀，cd 后面需要跟参数你不知道吗？");
-			
 		}
 		switch (param.get(0)) {
 		case ".":
+			
 			break;
 		case "..":
+			File f = new File(path);//将路径封装到一个文件里？？？？
+			path = f.getParent();
 			break;
-
 		default:
 			break;
 		}
@@ -90,12 +98,79 @@ public class Main {
 		System.exit(0);
 	}
 	private void ls() throws IOException{
-		path = new File("").getAbsolutePath();//获取当前目录
-		File f = new File(path); 
-		String[] names = f.list();
-		for(String name : names)//for 的高级循环
+			File f = new File(path);
+			String[] names = f.list();
+			for(String name : names)//for 的高级循环
+			{
+				System.out.println(name);
+			}
+	}
+/*列出当前路径*/
+	public void pwd() 
+	{
+		System.out.println(path);
+	}
+
+	public void copy()
+	{		
+		BufferedReader bufr = null;
+		BufferedWriter bufw = null;
+		String str = null;
+		BufferedReader br = null;
+		try
 		{
-			System.out.println(name);
+
+			if(param.isEmpty()){
+			System.out.println("有病是不是，会不会用呀，copy 后面需要跟参数你不知道吗？");
+			}
+			switch (param.get(0))
+			{
+			
+			case "copy.txt":
+					bufr = new BufferedReader(new FileReader("copy.txt"));
+					bufw = new BufferedWriter(new FileWriter("复制出来的文件.txt"));
+
+					String line = null;
+
+					while((line=bufr.readLine())!=null)
+					{
+						bufw.write(line);
+						bufw.newLine();
+						bufw.flush();
+
+					}
+				break;	
+			default :
+				System.out.println("你要复制的文件不存在！请查正再复制！");
+				break;
+			}
+
+			
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException("读写失败");
+		}
+		finally
+		{
+			try
+			{
+				if(bufr!=null)
+					bufr.close();
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException("读取关闭失败");
+			}
+			try
+			{
+				if(bufw!=null)
+					bufw.close();
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException("写入关闭失败");
+			}
 		}
 	}
 	public static void main(String[] args) throws IOException {
